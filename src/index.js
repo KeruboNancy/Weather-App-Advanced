e1d1f4581f813b04608c27b6a7cea1ea9b755606
KeruboNancy <163673453+KeruboNancy@users.noreply.github.com>
2024-05-27 02:50:28 +0300

    injected js to the code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-let forecast = document.querySelector("#forecast");
+function displayForecast() {
+    let forecast = document.querySelector("#forecast");
+    let days = [`Sun`, `Mon`, `Tue`, `Wed`, `Thu`, `Fri`, `Sat`]; // Include all days of the week
+    
+    forecast.innerHTML = '';
 
-forecast.innerHTML = `<div class="weather-forecast-day">
-        <div class="weather-forecast-date">Thu</div>
-
-        <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png" class="weather-forecast-icon">
-        <div class="weather-forecast-temperatures">
-          <div class="weather-forecast-temperature">
-            <strong>16º</strong>
-          </div>
-          <div class="weather-forecast-temperature">10º</div>
-        </div>`;
+    days.forEach(function(day) {
+        forecast.innerHTML += `
+            <div class="weather-forecast-day">
+                <div class="weather-forecast-date">${day}</div>
+                <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png" class="weather-forecast-icon">
+                <div class="weather-forecast-temperatures">
+                    <div class="weather-forecast-temperature">
+                        <strong>16º</strong>
+                    </div>
+                    <div class="weather-forecast-temperature">10º</div>
+                </div>
+            </div>`;
+    });
+}
+displayForecast();
